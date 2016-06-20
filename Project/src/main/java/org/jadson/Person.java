820131963818cc10b9c8743f45f39607197d1ecf
Jadson <jadsonjs@gmail.com>
2016-06-20 15:30:25 -0300

    #13434 - teste 2
    Signed-off-by: Jadson <jadsonjs@gmail.com>

--- Project/src/main/java/org/jadson/Person.java
+++ Project/src/main/java/org/jadson/Person.java
@@ -17,7 +17,7 @@ import org.apache.commons.collections.list.GrowthList;
 public class Person {
 	
     /** O nome da pessoa*/
-    private String name = "Teste";
+    private String name = "Teste 2.1";
     
     /* cpf da pessoa conflito aqui !!! */
     private String cpf;
