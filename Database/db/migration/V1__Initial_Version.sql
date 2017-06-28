6ef8c65ffb69b76b8eb8666efd75dff9a9a1ae76
Jadson Santos <jadsonjs@gmail.com>
2017-06-28 15:01:18 -0300

    #tarefa 3456893 -  apresentacao fluxo git

--- Database/db/migration/V1__Initial_Version.sql
+++ Database/db/migration/V1__Initial_Version.sql
@@ -3,3 +3,4 @@ insert into person (name) values('Joao');
 insert into person (name) values('Paulo');
 insert into person (name) values('Maria');
 insert into person (name) values('Ana');
+insert into person (name) values('Antonio');
